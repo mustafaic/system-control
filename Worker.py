c0a9ea4f5e752e42d2e88227d2001ec0887b4a72
Mustafa Tayyip İç <icmustafa1@gmail.com>
2025-08-05 11:35:57 +0300

    b101

--- Worker.py
+++ Worker.py
@@ -1,18 +1,22 @@
+import os
+
 import requests
 from PyQt5.QtCore import QThread
 from PyQt5.QtCore import pyqtSignal
-import time
+import ctypes
+import socket
 
 
 class Worker(QThread):
     update_signal = pyqtSignal(str, bool)
 
     def run(self):
+        unc_path = r"\\b101ftp\paylasim"
         while True:
             self.check_site("internet","https://www.google.com")
             self.check_site("website", "https://bolge01.dsi.gov.tr/")
             self.check_site("belgenet", "https://belgenet.dsi.gov.tr/edys-web/sistemeGiris.xhtml")
-            self.check_site("b101", "https://youtube.com/")
+            self.file_check(unc_path,"b101")
 
 
     def check_site(self, name, url):
@@ -24,4 +28,37 @@ class Worker(QThread):
             result = False
 
         self.update_signal.emit(name, result)
+
+
+    def file_check(self, unc_path: str, name):
+        if not unc_path.startswith(r"\\"):
+            print("Geçersiz UNC yolu formatı.")
+            return
+
+        try:
+            server_name = unc_path.split("\\")[2]
+        except IndexError:
+            print("UNC yolundan sunucu adı alınamadı.")
+            return
+
+        try:
+            socket.gethostbyname(server_name)
+        except socket.gaierror:
+            print("Sunucu adı çözülemedi. (Bağlantı sorunu veya yanlış ad)")
+            return
+
+        if os.path.exists(unc_path):
+            print("UNC klasörüne erişim başarılı.")
+            return True
+        else:
+            error_code = ctypes.windll.kernel32.GetLastError()
+
+            if error_code == 5:
+                print("Erişim reddedildi. (Yetki sorunu)")
+            elif error_code == 53:
+                print("Ağ yolu bulunamadı. (Sunucuya erişilemiyor)")
+            elif error_code == 67:
+                print("Ağ adı bulunamadı. (Yanlış paylaşım adı)")
+            else:
+                print(f"️ UNC yoluna erişilemedi. Hata kodu: {error_code}")
 
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
 import images_rc
 import webbrowser
 from Worker import Worker
 from datetime import datetime
+from test1 import Ui_MainWindow
+import os
+
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 
 class MyApp(QMainWindow):
@@ -14,17 +22,24 @@ class MyApp(QMainWindow):
 
         self.time = datetime.now()
 
-        uic.loadUi("test1.ui", self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowIcon(QIcon(resource_path("test.ico")))
+
         self.setWindowTitle("Sistem Kontrol")
 
         self.worker = Worker()
         self.worker.update_signal.connect(self.update_ui)
         self.worker.start()
 
-        self.google_git.clicked.connect(lambda: self.link_ac("https://www.google.com/"))
-        self.b101_button.clicked.connect(lambda: self.link_ac("https://www.youtube.com/"))
-        self.belgenet_button.clicked.connect(lambda: self.link_ac("https://belgenet.dsi.gov.tr/edys-web/sistemeGiris.xhtml"))
-        self.website_button.clicked.connect(lambda: self.link_ac("https://bolge01.dsi.gov.tr/"))
+        self.ui.google_git.clicked.connect(lambda: self.link_ac("https://www.google.com/"))
+        self.ui.b101_button.clicked.connect(lambda: self.link_ac("https://www.youtube.com/"))
+        self.ui.belgenet_button.clicked.connect(lambda: self.link_ac("https://belgenet.dsi.gov.tr/edys-web/sistemeGiris.xhtml"))
+        self.ui.website_button.clicked.connect(lambda: self.link_ac("https://bolge01.dsi.gov.tr/"))
+
+
+    def startMaximized(self):
+        self.showMaximized()
 
 
     def link_ac(self, url):
@@ -35,94 +50,94 @@ class MyApp(QMainWindow):
     def update_ui(self, site, status):
         if site == "internet":
             if status:
-                self.internet_card.setStyleSheet("#internet_card {\n"
+                self.ui.internet_card.setStyleSheet("#internet_card {\n"
                                       "    border: 10px solid rgb(46, 204, 113);\n"
                                       "    border-radius: 70px;\n"
                                       "    padding: 10px;\n"
                                       "}")
 
-                self.internet_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.internet_durum.setText("Durum: Çalışıyor")
+                self.ui.internet_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.internet_durum.setText("Durum: Çalışıyor")
 
             else:
-                self.internet_card.setStyleSheet("#internet_card {\n"
+                self.ui.internet_card.setStyleSheet("#internet_card {\n"
                                                  "    border: 10px solid red;\n"
                                                  "    border-radius: 70px;\n"
                                                  "    padding: 10px;\n"
                                                  "}")
 
-                self.internet_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.internet_durum.setText("Durum: Çalışmıyor")
+                self.ui.internet_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.internet_durum.setText("Durum: Çalışmıyor")
 
 
         elif site == "website":
             if status:
-                self.website_card.setStyleSheet("#website_card {\n"
+                self.ui.website_card.setStyleSheet("#website_card {\n"
                                       "    border: 10px solid rgb(46, 204, 113);\n"
                                       "    border-radius: 70px;\n"
                                       "    padding: 10px;\n"
                                       "}")
 
-                self.website_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.website_durum.setText("Durum: Çalışıyor")
+                self.ui.website_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.website_durum.setText("Durum: Çalışıyor")
 
             else:
-                self.website_card.setStyleSheet("#website_card {\n"
+                self.ui.website_card.setStyleSheet("#website_card {\n"
                                                 "    border: 10px solid red;\n"
                                                 "    border-radius: 70px;\n"
                                                 "    padding: 10px;\n"
                                                 "}")
 
-                self.website_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.website_durum.setText("Durum: Çalışmıyor")
+                self.ui.website_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.website_durum.setText("Durum: Çalışmıyor")
 
 
         elif site == "belgenet":
             if status:
-                self.belgenet_card.setStyleSheet("#belgenet_card {\n"
+                self.ui.belgenet_card.setStyleSheet("#belgenet_card {\n"
                                       "    border: 10px solid rgb(46, 204, 113);\n"
                                       "    border-radius: 70px;\n"
                                       "    padding: 10px;\n"
                                       "}")
 
-                self.belgenet_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.belgenet_durum.setText("Durum: Çalışıyor")
+                self.ui.belgenet_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.belgenet_durum.setText("Durum: Çalışıyor")
 
             else:
-                self.belgenet_card.setStyleSheet("#belgenet_card {\n"
+                self.ui.belgenet_card.setStyleSheet("#belgenet_card {\n"
                                                  "    border: 10px solid red;\n"
                                                  "    border-radius: 70px;\n"
                                                  "    padding: 10px;\n"
                                                  "}")
 
-                self.belgenet_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.belgenet_durum.setText("Durum: Çalışmıyor")
+                self.ui.belgenet_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.belgenet_durum.setText("Durum: Çalışmıyor")
 
 
         elif site == "b101":
             if status:
-                self.b101_card.setStyleSheet("#b101_card {\n"
+                self.ui.b101_card.setStyleSheet("#b101_card {\n"
                                       "    border: 10px solid rgb(46, 204, 113);\n"
                                       "    border-radius: 70px;\n"
                                       "    padding: 10px;\n"
                                       "}")
 
-                self.b101_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.b101_durum.setText("Durum: Çalışıyor")
+                self.ui.b101_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.b101_durum.setText("Durum: Çalışıyor")
 
             else:
-                self.b101_card.setStyleSheet("#b101_card {\n"
+                self.ui.b101_card.setStyleSheet("#b101_card {\n"
                                              "    border: 10px solid red;\n"
                                              "    border-radius: 70px;\n"
                                              "    padding: 10px;\n"
                                              "}")
 
-                self.b101_check.setText(datetime.now().strftime("%H:%M:%S"))
-                self.b101_durum.setText("Durum: Çalışmıyor")
+                self.ui.b101_check.setText(datetime.now().strftime("%H:%M:%S"))
+                self.ui.b101_durum.setText("Durum: Çalışmıyor")
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
-    window.show()
+    window.startMaximized()
     sys.exit(app.exec_())
